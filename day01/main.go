b93cb22a10904a605e83406acb6378e75206a292
Scott Nadeau <snadeau1@gmail.com>
2024-12-01 22:24:44 -0600

    day 01

---
+++ day01/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
+	left := make([]int, len(lines))
+	right := make([]int, len(lines))
+	for i := 0; i < len(lines); i++ {
+		nums := strings.Fields(lines[i])
+		l, err := strconv.Atoi(nums[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		left[i] = l
+		r, err := strconv.Atoi(nums[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		right[i] = r
+	}
+	fmt.Println("Distance:", solveDistance(left, right))
+	fmt.Println("Similarity:", solveSimilarity(left, right))
+}
+
+func Abs(value int) int {
+	if value < 0 {
+		return -value
+	}
+	return value
+}
+
+func solveDistance(left []int, right []int) int {
+	sort.Ints(left)
+	sort.Ints(right)
+	distance := 0
+	for i := 0; i < len(left); i++ {
+		distance += Abs(left[i] - right[i])
+	}
+	return distance
+}
+
+func solveSimilarity(left []int, right []int) int {
+	similarity := 0
+	freqMap := getFrequencyMap(right)
+	for _, value := range left {
+		similarity += freqMap[value] * value
+	}
+	return similarity
+}
+
+func getFrequencyMap(values []int) map[int]int {
+	freq := make(map[int]int)
+	for _, value := range values {
+		freq[value] += 1
+	}
+	return freq
+}
